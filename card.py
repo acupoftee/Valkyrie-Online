a99ca510fcfa7c3f1c461e43672d0f2786b72fe0
acupoftee <bdiakaba@uccs.edu>
2019-01-19 13:21:22 -0600

    username functionality

--- card.py
+++ card.py
@@ -10,13 +10,16 @@ fnt = ImageFont.truetype('fonts/Architex.ttf', 50)
 d = ImageDraw.Draw(txt)
 
 # draw text with half opacity()
-def print_text(username):
-    # hello text
-    d.text((15, 10), "Hello there\nnot_umce", font=fnt, fill=(255, 153, 153, 255))
-    d.text((15, 200), "You've got\nthis.", font=fnt, fill=(255, 153, 153, 255))
-    d.text((500, 310), "With love,\nValkyrie Online.", font=fnt, fill=(255, 153, 153, 255))
+# def print_text(username):
+#     # hello text
+#     d.text((15, 10), "Hello there\nnot_umce", font=fnt, fill=(255, 153, 153, 255))
+#     d.text((15, 200), "You've got\nthis.", font=fnt, fill=(255, 153, 153, 255))
+#     d.text((500, 310), "With love,\nValkyrie Online.", font=fnt, fill=(255, 153, 153, 255))
 
 
+d.text((15, 10), "Hello there\nnot_umce", font=fnt, fill=(255, 153, 153, 255))
+d.text((15, 200), "You've got\nthis.", font=fnt, fill=(255, 153, 153, 255))
+d.text((500, 310), "With love,\nValkyrie Online.", font=fnt, fill=(255, 153, 153, 255))
 out = Image.alpha_composite(base, txt)
 
 out.show()
